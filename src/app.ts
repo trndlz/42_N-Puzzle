aac3d99ef05d3cfaf88ed79cb6db8ff5b3846462
Trestan <trestan.mervin@gmail.com>
2019-06-18 22:44:56 +0200

    spiralArray target

--- src/app.ts
+++ src/app.ts
@@ -4,105 +4,59 @@ import cors from "cors";
 import express from "express";
 import routes from "./routes";
 
-function spiralFill(m): number[][] 
-{ 
-    // Initialize value to be filled  
-    // in matrix 
-    let val = 1;
-    let a = new Array(Number);
-  
-    /* k - starting row index 
-       m - ending row index 
-       l - starting column index 
-       n - ending column index */
-    let k = 0; 
-    let l = 0;
-    let n = m;
-    while (k < m && l < m) 
-    { 
-        /* Print the first row from 
-        the remaining rows */
-        for (let i = l; i < m; ++i) {
-            a[k][i] = val++; 
+const spiralArray = (n) => {
+    const result = new Array(n);
+    for (let p = 0; p < result.length; p++) {
+        result[p] = new Array(n);
+    }
+    let counter = 1;
+    let startCol = 0;
+    let endCol = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+    const total = n * n;
+    while (startCol <= endCol && startRow <= endRow) {
+        for (let i = startCol; i <= endCol; i++) {
+            result[startRow][i] = counter === total ? 0 : counter++;
         }
-            
-  
-        k++; 
-  
-        /* Print the last column from 
-        the remaining columns */
-        for (let i = k; i < n; ++i) {
-            a[i][m - 1] = val++; 
+        startRow++;
+        for (let j = startRow; j <= endRow; j++) {
+            result[j][endCol] = counter === total ? 0 : counter++;
         }
-            
-        n--; 
-  
-        /* Print the last row from  
-        the remaining rows */
-        if (k < m) 
-        { 
-            for (let i = m - 1; i >= l; --i) {
-                  a[m - 1][i] = val++; 
-            }
-                
-            m--; 
-        } 
-  
-        /* Print the first column from 
-           the remaining columns */
-        if (l < m) 
-        { 
-            for (let i = m - 1; i >= k; --i) {
-                  a[i][l] = val++; 
-            }
-            l++; 
+        endCol--;
+        for (let i = endCol; i >= startCol; i--) {
+            result[endRow][i] = counter === total ? 0 : counter++;
         }
-        return a;
-    } 
-} 
-// function spiralArray(test: number ): Array<number> {
+        endRow--;
+        for (let i = endRow; i >= startRow; i--) {
+            result[i][startCol] = counter === total ? 0 : counter++;
+        }
+        startCol++;
+    }
+    return result;
 
-//       const ids: Array<number> = [23, 34, 100, 124, 44]; 
-//       return ids
-// }
-
-// const spiralArray2: Array<number> = (test: number ) => {
-
-//       const ids: Array<number> = [23, 34, 100, 124, 44]; 
-//       return ids
-// }
-
-
-
-const test = (hello) => {
-
-      const st = hello.toString()
-      return st;
 }
-   
-//   // T E S T:
-//   arr = spiralArray(edge = 5);
-//   for (y= 0; y < edge; y++) console.log(arr[y].join(" "));
+
 
 class App {
 
-      public app: express.Application;
-      
-      constructor() {
-            this.app = express();
-            this.config();
-            const a = spiralFill(5)
-            a.forEach(i => console.log(i))
-      }
+    public app: express.Application;
 
-      
-      
-      private config(): void {
-            this.app.use(cors());
-            this.app.use(bodyParser.json());
-            this.app.use(bodyParser.urlencoded({extended: false}));
-            this.app.use("/api/v1", routes);
-      }
+    constructor() {
+        this.app = express();
+        this.config();
+        const a = spiralArray(5)
+        a.forEach(i => console.log(i))
+    }
+
+
+
+    private config(): void {
+        this.app.use(cors());
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use("/api/v1", routes);
+    }
 }
 
 export default new App().app;
