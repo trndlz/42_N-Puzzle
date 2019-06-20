a10fa7010e6269c4bc34bad7ff58d0291bc86897
Trestan <trestan.mervin@gmail.com>
2019-06-20 21:56:54 +0200

    Added manhattan + humming priorities

--- src/Puzzle.ts
+++ src/Puzzle.ts
@@ -1,17 +1,10 @@
-import flatten from "lodash/flatten"
-
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
+import flatten from "lodash/flatten";
+import isEqual from "lodash/isEqual";
+import flatMap from "lodash/flatMap"
 
 export interface IPuzzleInput {
     inputStr: string;
     heuristics: string;
-    size: number;
-    solvable?: boolean;
 }
 
 export type puzzleArray = number[][];
@@ -61,25 +54,98 @@ export default class NPuzzle {
         return newPuzzle;
     }
 
+    public addCoord(a: ICoord, b: ICoord): ICoord {
+        return {
+            "x": a.x + b.x,
+            "y": a.y + b.y
+        }
+    }
 
+    public arePuzzlesEqual(a: puzzleArray, b: puzzleArray): boolean {
+        return isEqual(a, b);
+    }
+
+    public nArrayToKeyCoord(puzzle: puzzleArray) {
+        const keyCoord = [];
+        puzzle.forEach((arr, y) => {
+            arr.forEach((val, x) => {
+                keyCoord[val] = {"x": x, "y": y};
+            })
+        })
+        return keyCoord ;
+    }
+
+
+    // Hamming priority function.
+    // The number of blocks in the wrong position + number of moves made so far
+    public hammingPriority(current: puzzleArray, target: puzzleArray, m: number): number {
+        let i = 0;
+        current.forEach((arr, y) => {
+            arr.forEach((val, x) => {
+                if (val !== 0 && val !== target[y][x]) {
+                    i++;
+                }
+            })
+        })
+        return (i + m);
+    }
+
+    public manhattanDistance(c1: ICoord, c2: ICoord): number {
+        return (Math.abs(c1.x - c2.x) + Math.abs(c1.y - c2.y))
+    }
+
+    // Manhattan priority function
+    // The sum of the Manhattan distances + number of moves made so far
+    public manhattanPriority(current: puzzleArray, target: puzzleArray, m: number): number {
+        let i = 0;
+        const targetCoordArr = this.nArrayToKeyCoord(target);
+        current.forEach((arr, y) => {
+            arr.forEach((val, x) => {
+                if (val !== 0 && val !== target[y][x]) {
+                    const targetCoord = targetCoordArr[val]
+                    i += this.manhattanDistance(targetCoord, {"x": x, "y": y});
+                }
+            })
+        })
+        return (i + m);
+    }
+    
+
+    public getNeighboursZero(puzzle: puzzleArray, initZero?: ICoord): ICoord[] {
+        const possibleMoves: ICoord[] = [
+            { "x": 0, "y": 1 }, // Right
+            { "x": -1, "y": 0 }, // Down
+            { "x": 0, "y": -1 }, // Left
+            { "x": 1, "y": 0 }, // Up
+        ]
+
+        const size = puzzle.length;
+        const neighbours = [];
+        const zero = initZero ? initZero : this.getZeroPosition(puzzle);
+        possibleMoves.forEach((dir) => {
+            const testCoord = this.addCoord(zero, dir);
+            if (testCoord.x >= 0 && testCoord.x < (size - 1) && testCoord.y >= 0 && testCoord.y < (size - 1)) {
+                neighbours.push(testCoord)
+            }
+        })
+        return neighbours;
+    }
 
     public getZeroPosition(puzzle: puzzleArray): ICoord {
-        let res: ICoord = {x: -1, y: -1}
+        let res: ICoord = { x: -1, y: -1 }
         puzzle.forEach((arr, y) => {
             arr.forEach((val, x) => {
                 if (val === 0) {
-                    res = {"x": x, "y": y}
+                    res = { "x": x, "y": y }
                 }
             })
         })
         return res
     }
 
-
-
     private isForbidenChars(lines: string[]): boolean {
         // For lines not starting with # : check if any other character than whitespace or digit is present
-       return lines.filter(line => (line[0] !== "#" && /[^\d\s]/g.test(line))).length > 0
+        return lines.filter(line => (line[0] !== "#" && /[^\d\s]/g.test(line))).length > 0
     }
 
     private isUnexpectedNumbers(puzzle: puzzleArray, size?: number): boolean {
--- src/app.ts
+++ src/app.ts
@@ -4,36 +4,45 @@ import cors from "cors";
 import express from "express";
 import routes from "./routes";
 import { spiralArray } from "./components/spiralArray";
-import NPuzzle from "./Puzzle";
+import NPuzzle, { puzzleArray } from "./Puzzle";
 import fs from "fs";
 
 class App {
 
     public app: express.Application;
 
+   
+
     constructor() {
         this.app = express();
         this.config();
-        const a = spiralArray(5)
-        // a.forEach(i => console.log(i))
+        const a = spiralArray(4)
 
+        
         const test = new NPuzzle({
             inputStr: "1 2 3 4 5 6 7 8 0",
             heuristics: "manhattan",
-            size: 8,
         })
 
         const bbb = fs.readFileSync("test1").toString("utf-8");
+        const ccc = fs.readFileSync("test2").toString("utf-8");
         // console.log(bbb)
         const ba = test.parseInputString(bbb)
-        console.log(ba)
-        const aze = test.getZeroPosition(ba)
-        const ppp = test.createNewPuzzle(ba, aze, {"x": 1, "y": 1})
-        console.log(ppp)
+        const cb = test.parseInputString(ccc)
+        // console.log(ba)
+        // console.log(test.getNeighboursZero(ba))
+        // console.log(cb)
+        // console.log(test.getNeighboursZero(cb))
+        console.log(test.hammingPriority(ba, cb, 0))
+        console.log(test.manhattanPriority(ba, cb, 0))
+
+
+        
+
         // console.log(aze)
 
         
-    }
+    }  
 
     private config(): void {
         this.app.use(cors());
